1625c6d24d103c092e8a2b399fafb8e3978f0e9e
bsibdev <71753160+bsibdev@users.noreply.github.com>
2024-07-22 21:06:34 -0400

    add row deletion

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-const library = [];
+const library = [
+    {
+    id:"1",
+    title:'The Old Man and The Sea',
+    author:'Ernest Hemmingway',
+    pages:'128',
+    read: 'Unread'}
+    ,
+    {
+    id:"2",
+    title:'Maximum Ride: The Angel Experiment',
+    author:'James Patterson',
+    pages:'464',
+    read:'read'
+    }
+];
 
 function Book(id,title,author,pages,read) {
     this.id = id;
@@ -6,6 +21,37 @@ function Book(id,title,author,pages,read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
+}
+
+for (let i = 0; library.length>i; i++) {
+    const libraryTable = document.querySelector('#library-table');
+    const newRow = document.createElement('tr');
+    const newTitle = document.createElement('th');
+    const newAuthor = document.createElement('th');
+    const newPages = document.createElement('th');
+    const newRead = document.createElement('th');
+    const del = document.createElement('th');
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'X';
+    deleteButton.setAttribute("id",`del${i}`)
+    del.appendChild(deleteButton);
+
+    deleteButton.addEventListener('click', () => {
+        deleteButton.parentNode.parentNode.remove();
+    })
+
+    newTitle.textContent = library[i].title;
+    newAuthor.textContent = library[i].author;
+    newPages.textContent = library[i].pages;
+    newRead.textContent = library[i].read;
+    
+    newRow.appendChild(newTitle);
+    newRow.appendChild(newAuthor);
+    newRow.appendChild(newPages);
+    newRow.appendChild(newRead);
+    newRow.appendChild(del);
+    libraryTable.appendChild(newRow);
 }
 
 function addBook(){
@@ -35,6 +81,10 @@ function addBook(){
     deleteButton.textContent = 'X';
     del.appendChild(deleteButton);
 
+    deleteButton.addEventListener('click', () => {
+        deleteButton.parentNode.parentNode.remove();
+    })
+
     newTitle.textContent = newBook.title;
     newAuthor.textContent = newBook.author;
     newPages.textContent = newBook.pages;
@@ -46,7 +96,7 @@ function addBook(){
     newRow.appendChild(newRead);
     newRow.appendChild(del);
     libraryTable.appendChild(newRow);
-
+    
 
 }
 
@@ -55,3 +105,8 @@ document.addEventListener("submit",(event) => {
     event.preventDefault();
     addBook();
 })
+
+//add delete functionality.
+
+
+
